1557bc69a2489d937ff4501f30e7055162c872ae
Yvonne Chen <yvonnechen1991@gmail.com>
2021-01-12 19:59:11 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end 
+
+    def add_student(student, grade)
+        if !@roster[grade]
+        @roster[grade] = [];
+        end 
+        @roster[grade].push(student)    
+    end 
+
+    def grade(grade)
+        @roster[grade]
+    end 
+
+    def sort
+        @roster.each do |grade, students|
+            students.sort!
+        end 
+        @roster
+        # @roster.map do |grade, students|
+        #     [grade, students.sort]
+        # end.to_h
+    end 
+end 
